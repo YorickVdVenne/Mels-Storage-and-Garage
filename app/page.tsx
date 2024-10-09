ada0e823ef7982b9e6ef23fcc5343295576eaf79
Yorick-Level-Level <yorick@level-level.com>
2024-10-09 20:18:32 +0200

    feat: adds meta data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import Header from '@/components/organisms/Header/Component';
+import type { Metadata } from 'next';
+
 import About from '@/components/organisms/About/Component';
 import Contact from '@/components/organisms/Contact/Component';
+import Header from '@/components/organisms/Header/Component';
 import Rates from '@/components/organisms/Rates/Component';
+
+export const metadata: Metadata = {
+  title: 'Mels - Washing and Detailing',
+  description: 'Mels - Washing and Detailing',
+  openGraph: {
+    title: "Mel's Washing and Detailing",
+    description: "Mel's Washing and Detailing",
+    images: '/default/android-chrome-192x192.png',
+  },
+};
 
 export default function Home() {
   return (
---
+++ components/molecules/Favicons/Component.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function Favicons(): JSX.Element {
+  return (
+    <head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/default/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/default/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/default/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/default/favicon-48x48.png"
+        sizes="48x48"
+      />
+      <link
+        rel="mask-icon"
+        href="/default/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <link rel="shortcut icon" href="/default/favicon.ico" />
+      <link rel="icon" type="image/svg+xml" href="/default/favicon.svg" />
+      <meta name="msapplication-TileColor" content="#082866" />
+      <meta name="msapplication-config" content="/default/browserconfig.xml" />
+      <meta name="theme-color" content="#082866" />
+      <link rel="manifest" href="/default/site.webmanifest" />
+    </head>
+  );
+}
